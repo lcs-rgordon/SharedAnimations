03665641b505600fd38de0fb108461db6b5e3e8c
Russell Gordon <github@russellgordon.ca>
2022-04-27 06:15:03 -0400

    Initial Commit

---
+++ SharedAnimations/SharedAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedAnimationsApp.swift
+//  SharedAnimations
+//
+//  Created by Russell Gordon on 2022-04-27.
+//
+
+import SwiftUI
+
+@main
+struct SharedAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
